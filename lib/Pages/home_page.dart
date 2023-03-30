3053e68119b2574ba6b28ec3cbd04c1bfcc377b7
Owen Poling <owenpoling21@gmail.com>
2023-03-29 22:20:57 -0400

    Created a new homepage with a todo_list.dart util

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,1 +1,28 @@
-// This is the home page:
+// this is Owen's feature:
+import 'package:flutter/material.dart';
+import 'package:ribit/Pages/util/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF447604),
+      appBar: AppBar(
+        title: Text('To Do'),
+        elevation: 0,
+      ),
+      body: ListView(
+        children: [
+          ToDoTile(),
+        ],
+      ),
+    );
+  }
+}
