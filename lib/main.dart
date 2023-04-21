ed0eb4c5f0835688c6a1f553f8386ff658e54d61
Owen Poling <owenpoling21@gmail.com>
2023-04-21 11:25:47 -0400

    delete home_page

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //home: LoginPage(),
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
