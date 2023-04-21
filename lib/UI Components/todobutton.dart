dbce01f104096d25968e96f63332e04d29f12010
Osaze Imohe <77579038+osazeimohe@users.noreply.github.com>
2023-04-21 14:54:17 -0400

    Revert "Revert "Created a new homepage with a todo_list.dart util""
    This reverts commit ada69763acc190f3f9a9f765e80ffd250d2ae033.

---
+++ lib/UI Components/todobutton.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class todobutton extends StatelessWidget {
+  final Function()? onTap;
+  const todobutton({
+    super.key,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: Colors.green,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: const Center(
+          child: Text(
+            "Task List",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/UI Components/todocolors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFDA4040);
+const Color tdBlue = Color(0xFFCFE795);
+
+const Color tdBlack = Colors.white;
+const Color tdGrey = Colors.black;
+
+const Color tdBGColor = Color(0xFFCFE795);
